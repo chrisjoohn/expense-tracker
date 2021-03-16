6d35d44d7ea78420e03e0aafe25b632e475cfe35
Chris John Mulingbayan <chrisjohn@Chriss-MacBook-Air.local>
2021-03-17 03:01:00 +0800

    Added passportjs dependencies and setup passport config

---
+++ api/config/passport.js
@@ -0,0 +1,46 @@
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const JwtStrategy = require("passport-jwt").Strategy;
+const ExtractJwt = require("passport-jwt").ExtractJwt;
+
+const UserModel = require("../models/user");
+
+passport.use(
+  new LocalStrategy({ usernameField: "email", password: "password" }, function (
+    username,
+    password,
+    done
+  ) {
+    UserModel.findOne({ email: username }, function (err, user) {
+      if (err) return done(err, false);
+      if (!user) return done(null, false);
+
+      user.comparePassword(password, function (err, isMatch) {
+        if (err) return done(err, false);
+        if (!isMatch) return done(null, false);
+        return done(null, user);
+      });
+    });
+  })
+);
+
+passport.use(
+  new JwtStrategy(
+    {
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: "TOKEN_SECRET_KEY",
+    },
+    function (tokenPayload, done) {
+      const { id } = tokenPayload;
+      UserModel.findById(id, function (err, user) {
+        if (err) return done(err, false);
+        if (!user) return done(null, false);
+        return done(null, user);
+      });
+    }
+  )
+);
+
+
+module.exports = passport;
+
