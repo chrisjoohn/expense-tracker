c3deeda2735a6f39aa7123e11194c68a4579590a
Chris John Mulingbayan <chrisjohn@Chriss-MacBook-Air.local>
2021-03-24 16:01:39 +0800

    Setup sendgrid to send emails

---
+++ api/utils/emailHandler.js
@@ -0,0 +1,26 @@
+const sgMail = require("@sendgrid/mail");
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
+console.log(process.env.SENDGRID_API_KEY);
+
+const sendEmail = (recipient, subject, emailContent) => {
+  const msg = {
+    to: recipient,
+    from: "expense.dev@dispostable.com",
+    subject,
+    html: emailContent,
+  };
+
+  sgMail
+    .send(msg)
+    .then(() => {
+      console.log("Email sent!");
+    })
+    .catch((err) => {
+      console.log(err.response.body);
+    });
+};
+
+module.exports = {
+  sendEmail,
+};
