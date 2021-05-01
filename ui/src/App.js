100430a866973e92c1f5c8ffa48846d96cae838e
Chris John Mulingbayan <chrisjohnmulingbayan@gmail.com>
2021-05-01 19:52:12 +0800

    Added register page

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,12 +1,14 @@
 import { Switch, Route, Redirect } from "react-router-dom";
 
 import Login from "pages/Login";
+import Register from "pages/Register";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 
 const App = () => {
   return (
     <Switch>
+      <Route path="/register" component={Register} />
       <Route path="/login" component={Login} />
       <Route>
         <Redirect from="/" to="/login" />
