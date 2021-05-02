0f5d7eed28ee73b8dd4f89e6d094bfa6cfabd7c8
Chris John Mulingbayan <chrisjohnmulingbayan@gmail.com>
2021-05-02 17:28:55 +0800

    Added forgot password page

--- ui/src/App.js
+++ ui/src/App.js
@@ -2,12 +2,14 @@ import { Switch, Route, Redirect } from "react-router-dom";
 
 import Login from "pages/Login";
 import Register from "pages/Register";
+import ForgotPassword from "pages/ForgotPassword";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 
 const App = () => {
   return (
     <Switch>
+      <Route path="/forgot-password" component={ForgotPassword} />
       <Route path="/register" component={Register} />
       <Route path="/login" component={Login} />
       <Route>
