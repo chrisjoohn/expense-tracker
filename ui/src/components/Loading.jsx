dadfb674d1b208372fce0ab925ae95af91ffbec2
Chris John Mulingbayan <chrisjohnmulingbayan@gmail.com>
2021-07-20 14:34:48 +0800

    Fixed loading component

--- ui/src/components/Loading.jsx
+++ ui/src/components/Loading.jsx
@@ -1,13 +1,11 @@
 const Loading = (props) => {
   return (
-    <div style={{ position: "absolute", left: "50%", top: "40%" }}>
+    <div className="centered">
       <img
+        className="centered-child"
         src="/images/green-loading.gif"
         style={{
           width: "100px",
-          position: "relative",
-          left: "-50%",
-          top: "-50%",
         }}
       />
     </div>
