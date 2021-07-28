b286e6e791709b242bd2bb3bfe11b26bd26f7b00
Chris John Mulingbayan <chrisjohnmulingbayan@gmail.com>
2021-07-28 12:19:43 +0800

    Used single env file for both api and ui container

--- api/server.js
+++ api/server.js
@@ -14,6 +14,6 @@ app.use(passport.initialize());
 
 app.use("/api/v1", routes);
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.API_PORT || 8080;
 
 app.listen(PORT, () => console.log(`Server running on port: ${PORT}...`));
