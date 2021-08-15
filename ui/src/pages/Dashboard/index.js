e06cc3720d656135b484283b3ca9580619a052bd
Chris John Mulingbayan <chrisjohnmulingbayan@gmail.com>
2021-08-15 13:17:44 +0800

    Added expense chart

--- ui/src/pages/Dashboard/index.js
+++ ui/src/pages/Dashboard/index.js
@@ -1,21 +1,73 @@
 import styled from "styled-components";
+import { PieChart, Pie, Legend, Tooltip, ResponsiveContainer } from "recharts";
 
 import ExpenseContainer from "components/Expenses/ExpenseContainer";
 
 const ExpenseWrapper = styled.div`
-  height: 100vh;
-  background-color: #f3f3f3;
   display: flex;
   justify-content: space-around;
-  padding-left: 15rem;
-  padding-right: 15rem;
+  padding-left: 20rem;
+  padding-right: 20rem;
 `;
 
-const DashboardWrapper = styled.div``;
+const DashboardWrapper = styled.div`
+  height: 100vh;
+  background-color: #f3f3f3;
+`;
+
+const ChartWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  position: relative;
+`;
+
+const ChartDetail = styled.div`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  text-align: center;
+`;
+
+const Span = styled.span`
+  display: block;
+  font-weight: ${({ bold }) => (bold ? "bold" : "normal")};
+  font-size: ${({ size }) => (size ? size : 14) + "px"};
+`;
+
+const data = [
+  { name: "Unpaid Expenses", value: 7000, fill: "#9ed" },
+  { name: "Paid Expenses", value: 500, fill: "#0ca" },
+];
 
 const Dashboard = (props) => {
   return (
     <DashboardWrapper>
+      <ChartWrapper>
+        <ChartDetail>
+          <Span bold size={35}>
+            {" "}
+            7500
+          </Span>
+          <Span>Total Expenses</Span>
+          <Span bold size={25}>
+            500
+          </Span>
+          <Span>Total Expenses Paid</Span>
+        </ChartDetail>
+        <PieChart width={500} height={500}>
+          <Tooltip />
+          <Pie
+            dataKey="value"
+            data={data}
+            cx="50%"
+            cy="50%"
+            innerRadius={180}
+            outerRadius={220}
+            fill="#8884d8"
+          />
+        </PieChart>
+      </ChartWrapper>
       <ExpenseWrapper>
         <ExpenseContainer title="Fixed Expenses" />
         <ExpenseContainer title="Other Expenses" />
