e32240e6dbc5b1b8a1876c9c10e1155b7b60394b
Chris John Mulingbayan <chrisjohnmulingbayan@gmail.com>
2021-08-30 15:02:06 +0800

    Setup files needed for fixed-expenses integration

--- ui/src/store/reducers/root.js
+++ ui/src/store/reducers/root.js
@@ -2,10 +2,12 @@ import { combineReducers } from "redux";
 
 import AuthReducer from "./AuthReducer";
 import ExpenseReducer from "./ExpenseReducer";
+import FixedExpenseReducer from "./FixedExpenseReducer";
 
 const rootReducer = combineReducers({
   auth: AuthReducer,
   expense: ExpenseReducer,
+  fixedExpense: FixedExpenseReducer,
 });
 
 export default rootReducer;
