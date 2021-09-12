923c256fb44aa499267d1bc7fb94a48b1ac2b00c
Chris John Mulingbayan <chrisjohnmulingbayan@gmail.com>
2021-09-12 11:45:57 +0800

    Added payables route

--- api/routes/fixedExpenses.js
+++ api/routes/fixedExpenses.js
@@ -2,6 +2,7 @@ const router = require("express").Router();
 
 const FixedExpenseController = require("../controllers/fixedExpenses");
 
+router.get("/payables", FixedExpenseController.getPayables);
 router.post("/", FixedExpenseController.create);
 router.get("/:id", FixedExpenseController.findById);
 router.get("/", FixedExpenseController.findAll);
