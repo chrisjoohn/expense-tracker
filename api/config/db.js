5bdebe1cdd16cbb9f654292f7397590caf7743b7
Chris John Mulingbayan <chrisjohnmulingbayan@gmail.com>
2022-02-19 14:51:55 +0800

    Added remote db connection

--- api/config/db.js
+++ api/config/db.js
@@ -4,7 +4,12 @@ const DB_HOST = process.env.DB_HOST;
 const DB_NAME = process.env.DB_NAME;
 const DB_PORT = process.env.DB_PORT;
 
-mongoose.connect(`mongodb://${DB_HOST}:${DB_PORT}/${DB_NAME}`, {
+const DB_URI =
+  process.env.NODE_ENV === "development"
+    ? `mongodb://${DB_HOST}:${DB_PORT}/${DB_NAME}`
+    : `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@expenstracker.znmn0.mongodb.net/${DB_NAME}`;
+
+mongoose.connect(DB_URI, {
   useUnifiedTopology: true,
   useNewUrlParser: true,
   useCreateIndex: true,
